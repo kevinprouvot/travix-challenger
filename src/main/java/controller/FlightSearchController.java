c26c8b59354cc0d4696781d60d81ad97671ac6dd
KnProuvot <kroarr@mail.com>
2017-01-15 21:55:40 +0000

    Added /v1/ in the rest url

--- src/main/java/controller/FlightSearchController.java
+++ src/main/java/controller/FlightSearchController.java
@@ -1,13 +1,19 @@
 package main.java.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import main.java.controller.dto.FlightSearchDTO;
 
 @RestController
 public class FlightSearchController {
 
-	@RequestMapping("/")
-	public String search() {
+	@RequestMapping("/v1/search")
+	public String search(HttpServletRequest request, FlightSearchDTO flightSearchDTO) {
+		
+		
 		return "Work in progress";
 	}
 	
