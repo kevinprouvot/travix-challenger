6b93d7f58237d63ff68d68fc8dc0fc9f345df559
Kevin Prouvot <kevin.prouvot@bcsg.com>
2017-01-30 12:45:33 +0000

    Added FlightService

--- src/main/java/service/FlightService.java
+++ src/main/java/service/FlightService.java
@@ -1,5 +1,9 @@
 package main.java.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import main.java.adapter.AdapterFactory;
 import main.java.model.TravixRequest;
 import main.java.model.TravixResponse;
@@ -9,33 +13,67 @@ import main.java.service.supplier.exception.GenericRestException;
 import main.java.service.supplier.model.CrazyAirRequest;
 import main.java.service.supplier.model.CrazyAirResponse;
 import main.java.service.supplier.model.ToughJetRequest;
+import main.java.service.supplier.model.ToughJetResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FlightService {
-	
+
 	@Autowired
 	private CrazyAirRestService crazyAirRestService;
-	
+
 	@Autowired
 	private ToughJetRestService toughJetRestService;
-	
+
 	@Autowired
 	private AdapterFactory adapterFactory;
-	
-	public TravixResponse searchForFlight(TravixRequest request) {
+
+	public List<TravixResponse> searchForFlight(TravixRequest request) {
+		List<TravixResponse> adaptedCAresponses = new ArrayList<TravixResponse>();
+		
 		try {
-			CrazyAirRequest crazyAirRequest = adapterFactory.adaptFromTo(request, CrazyAirRequest.class);
-			CrazyAirResponse crazyAirResponse = crazyAirRestService.getCrazyAirResponse(crazyAirRequest);
+			CrazyAirRequest crazyAirRequest = adapterFactory.adaptFromTo(
+					request, CrazyAirRequest.class);
 			
-			ToughJetRequest toughJetRequest = adapterFactory.adaptFromTo(request, ToughJetRequest.class);
+			List<CrazyAirResponse> crazyAirResponses = crazyAirRestService
+					.getCrazyAirResponse(crazyAirRequest);
+
+			adaptedCAresponses = crazyAirResponses
+					.stream()
+					.map(czResponse -> adapterFactory.adaptFromTo(czResponse,
+							TravixResponse.class)).collect(Collectors.toList());
 			
+		} catch (GenericRestException e) {
+			//TODO handle recovery method
 		}
-		catch (GenericRestException e) {
+		
+		List<TravixResponse> adaptedTJresponses = new ArrayList<TravixResponse>();
+		
+		try {
+
+			ToughJetRequest toughJetRequest = adapterFactory.adaptFromTo(
+					request, ToughJetRequest.class);
 			
+			List<ToughJetResponse> toughJetResponses = toughJetRestService
+					.getToughjetResponse(toughJetRequest);
+
+			adaptedTJresponses = toughJetResponses
+					.stream()
+					.map(tjResponse -> adapterFactory.adaptFromTo(tjResponse,
+							TravixResponse.class)).collect(Collectors.toList());
+			
+		} catch (GenericRestException e) {
+			//TODO handle recovery method
 		}
+		
+		List<TravixResponse> responses = new ArrayList<TravixResponse>();
+		responses.addAll(adaptedCAresponses);
+		responses.addAll(adaptedTJresponses);
+		
+		return responses;
+
 	}
-	
+
 }
