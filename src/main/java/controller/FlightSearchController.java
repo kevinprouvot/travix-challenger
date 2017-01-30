bfd1b290efc6ff036b5f6e0121c129dc0b13b1d3
Kevin Prouvot <kevin.prouvot@bcsg.com>
2017-01-30 12:52:52 +0000

    Implemented FlishSearchController

--- src/main/java/controller/FlightSearchController.java
+++ src/main/java/controller/FlightSearchController.java
@@ -1,20 +1,27 @@
 package main.java.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import main.java.model.TravixRequest;
+import main.java.model.TravixResponse;
+import main.java.service.FlightService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FlightSearchController {
 
+	@Autowired
+	private FlightService flightService;
+	
 	@RequestMapping("/v1/search")
-	public String search(HttpServletRequest request, TravixRequest travixRequest) {
+	public List<TravixResponse> search(HttpServletRequest request, TravixRequest travixRequest) {
 		
-		
-		return "Work in progress";
+		return flightService.searchForFlight(travixRequest);
 	}
 	
 }
